3d741fdafb03b74b70435eb775850c3c29ff3277
Zeeshan <equbalzeeshan@gmail.com>
2022-01-19 23:26:57 +0530

    [COMPLETE]Matrix: Solved 9 questions

---
+++ Matrix/Row with max 1s.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+typedef long long ll;
+typedef long double ld;
+struct custom_hash;
+#define ff first
+#define I insert
+#define ss second
+#define maxN 300002
+#define inf INT_MAX
+#define ninf INT_MIN
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define pi 3.14159265
+#define ub upper_bound
+#define lb lower_bound
+#define popf pop_front
+#define mod 1000000007
+#define vi vector<int> 
+#define mll map<ll, ll>
+#define vll vector <ll>
+const int dx4[]={-1,0,1,0};
+const int dy4[]={0,1,0,-1};
+#define vvi vector<vector<int> >
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+const int dx8[]={-1,-1,-1,0,1,1,1,0};
+const int dy8[]={-1,0,1,1,1,0,-1,-1};
+#define ust unordered_set<ll, custom_hash>
+#define rep(i, a, b) for(ll i = a; i < ll(b); i++)
+#define umll unordered_map<ll, ll, custom_hash>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+struct custom_hash { size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM =
+chrono::steady_clock::now().time_since_epoch().count();x ^= FIXED_RANDOM;return x ^ (x >> 16);}};
+ll power(ll x,ll n) {ll result=1; while(n) {if(n%2==1) result=result*x; n=n/2; x=x*x;} return result;}
+ll gcd(ll a, ll b) {return b ? gcd(b, a%b) : a;} 
+ll lcm(ll a , ll b){return (a*b)/gcd(a,b);}
+ll BS(ll a[],ll s,ll n,ll val) {ll mid,beg=s,end=n-1; while(beg<=end)
+{mid=(beg+end)/2; if(val==a[mid]){break;} else if(val>a[mid]){beg=mid+1;}else{end=mid-1;}} return mid;}
+inline ll mul(ll x,ll y,ll m){ll z=1LL*x*y;if (z>=m){z%=m;} return z;}
+ll powmod(ll x,ll y,ll m){ll r=1;while(y){if(y&1){r=mul(r,x,m);}y>>=1;x=mul(x,x,m);}return r;}
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-result"
+
+//========================================XXXXXXXXXXXXXXXX=======================================
+
+void solve() {
+	int m, n;
+	cin >> m >> n;
+	vvi arr(m, vi(n));
+	rep(i, 0, m) {
+		rep(j, 0, n) {
+			cin >> arr[i][j];
+		}
+	}
+
+	// Naive [O(n*m)]
+	int row_idx = -1, min_cnt = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        int cnt = 0;
+        for(int j = 0; j < m; j++) {
+            if(arr[i][j] == 1) break;
+            else cnt++;
+        }
+        if(cnt < min_cnt && cnt != m) {
+            min_cnt = cnt;
+            row_idx = i;
+        }
+    }
+
+
+    // Optimal [O(n+m)]
+    int i = 0, j = m-1, row_idx = -1;
+    while(i < n && j >= 0) {
+    	if(arr[i][j] == 1) {
+    		row_idx = i;
+    		j--;
+    	}
+    	else
+    		i++;
+    }
+
+    cout << row_idx;
+}
+
+int main() {
+	fast;
+	#ifndef ONLINE_JUDGE
+  		freopen("../input.txt", "r", stdin);
+  		freopen("../output.txt", "w", stdout);
+	#endif
+	int t = 1;
+	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+#pragma GCC diagnostic pop

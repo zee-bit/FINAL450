e71042bd1dd263334c8b3a742e31162a42fb0400
Zeeshan <equbalzeeshan@gmail.com>
2022-02-09 18:19:44 +0530

    BinaryTrees: Solved 12 questions.

---
+++ BinaryTrees/Left view of binary tree.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+typedef long long ll;
+typedef long double ld;
+struct custom_hash;
+#define ff first
+#define I insert
+#define ss second
+#define maxN 300002
+#define inf INT_MAX
+#define ninf INT_MIN
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define pi 3.14159265
+#define ub upper_bound
+#define lb lower_bound
+#define popf pop_front
+#define mod 1000000007
+#define vi vector<int> 
+#define mll map<ll, ll>
+#define vll vector <ll>
+const int dx4[]={-1,0,1,0};
+const int dy4[]={0,1,0,-1};
+#define vvi vector<vector<int> >
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+const int dx8[]={-1,-1,-1,0,1,1,1,0};
+const int dy8[]={-1,0,1,1,1,0,-1,-1};
+#define ust unordered_set<ll, custom_hash>
+#define rep(i, a, b) for(ll i = a; i < ll(b); i++)
+#define umll unordered_map<ll, ll, custom_hash>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+struct custom_hash { size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM =
+chrono::steady_clock::now().time_since_epoch().count();x ^= FIXED_RANDOM;return x ^ (x >> 16);}};
+ll power(ll x,ll n) {ll result=1; while(n) {if(n%2==1) result=result*x; n=n/2; x=x*x;} return result;}
+ll gcd(ll a, ll b) {return b ? gcd(b, a%b) : a;} 
+ll lcm(ll a , ll b){return (a*b)/gcd(a,b);}
+ll BS(ll a[],ll s,ll n,ll val) {ll mid,beg=s,end=n-1; while(beg<=end)
+{mid=(beg+end)/2; if(val==a[mid]){break;} else if(val>a[mid]){beg=mid+1;}else{end=mid-1;}} return mid;}
+inline ll mul(ll x,ll y,ll m){ll z=1LL*x*y;if (z>=m){z%=m;} return z;}
+ll powmod(ll x,ll y,ll m){ll r=1;while(y){if(y&1){r=mul(r,x,m);}y>>=1;x=mul(x,x,m);}return r;}
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-result"
+
+//========================================XXXXXXXXXXXXXXXX=======================================
+
+struct Node
+{
+    int val;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        val = x;
+        left = right = NULL;
+    }
+};
+
+Node *buildTree() {
+	Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->right->left->left = new Node(7);
+    root->right->left->right = new Node(8);
+    return root;
+}
+
+// METHOD-1
+int max_level = 0;
+vector<int> res;
+
+void leftView(Node *node, int level) {
+	if(node == NULL)
+		return;
+
+	if(level > max_level) {
+		max_level = level;
+		res.push_back(node -> val);
+	}
+	leftView(node -> left, level + 1);
+	leftView(node -> right, level + 1);
+}
+
+// METHOD-2
+void leftView2(Node *node) {
+	if(node == NULL)
+		return;
+
+	queue<Node *> q;
+	q.push(node);
+
+	while(!q.empty()) {
+		int sz = q.size();
+		for(int i = 0; i < sz; i++) {
+			Node *curr = q.front(); q.pop();
+			if(i == 0) res.push_back(curr -> val);
+
+			if(curr -> left) q.push(curr -> left);
+			if(curr -> right) q.push(curr -> right);
+		}
+	}
+}
+
+void solve() {
+
+	// Implement build tree method
+	Node* root = buildTree();
+
+	leftView(root, 1);
+	for(auto node : res) cout << node << " ";
+}
+
+int main() {
+	fast;
+	#ifndef ONLINE_JUDGE
+  		freopen("../input.txt", "r", stdin);
+  		freopen("../output.txt", "w", stdout);
+	#endif
+	int t = 1;
+	// cin >> t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+#pragma GCC diagnostic pop
